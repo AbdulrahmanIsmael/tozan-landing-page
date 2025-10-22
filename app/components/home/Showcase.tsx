19974fbf3856d5b7b0d5bce830352340aebd60af
Abdulrahman Ismael <116180351+AbdulrahmanIsmael@users.noreply.github.com>
2025-10-22 20:50:06 +0300

    fix: fix showcase heading size

--- app/components/home/Showcase.tsx
+++ app/components/home/Showcase.tsx
@@ -28,12 +28,12 @@ export default function Showcase() {
       >
         {textInArray.map((l, i) =>
           l === " " ? (
-            <span className="font-semibold text-5xl md:text-9xl" key={i}>
+            <span className="font-semibold text-4xl md:text-7xl" key={i}>
               &nbsp;
             </span>
           ) : (
             <motion.span
-              className="font-semibold text-5xl md:text-9xl"
+              className="font-semibold text-4xl md:text-7xl"
               key={i}
               variants={{
                 hidden: { opacity: 0, y: -100 },
